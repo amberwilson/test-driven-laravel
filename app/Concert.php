d80fc017193e3fc7fab4b5e48654ea7e209357ac
amberwilson <678866+amberwilson@users.noreply.github.com>
2020-08-15 13:54:05 -0300

    Refactor "start time" and "price" • Add `ConcertTest::can_get_formatted_start_time()` and backing functionality • Add `ConcertTest::can_get_ticket_price_in_dollars()` and backing functionality • Update the view to leverage new accessors for formatting • Remove the `DatabaseMigrations` trait from `ConcertTest` and switch factory `create()` calls to `make()` calls since we don't need the DB for these tests • COVERS: https://course.testdrivenlaravel.com/lessons/module-1/refactoring-for-speed#5

--- app/Concert.php
+++ app/Concert.php
@@ -12,4 +12,12 @@ class Concert extends Model {
     public function getFormattedDateAttribute(): string {
         return $this->date->format('F j, Y');
     }
+
+    public function getFormattedStartTimeAttribute(): string {
+        return $this->date->format('g:ia');
+    }
+
+    public function getTicketPriceInDollarsAttribute(): string {
+        return number_format($this->ticket_price / 100, 2);
+    }
 }
--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -4,22 +4,27 @@ namespace Tests\Unit;
 
 use App\Concert;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ConcertTest extends TestCase {
-
-    use DatabaseMigrations;
-
     /** @test */
     public function can_get_formatted_date(): void {
-        // Create a concert with a known date
-        $concert = factory(Concert::class)->create(['date' => Carbon::parse('2020-08-15 8:00pm')]);
+        $concert = factory(Concert::class)->make(['date' => Carbon::parse('2020-08-15 8:00pm')]);
 
-        // Retrieve formatted date
-        $date = $concert->formatted_date;
+        self::assertEquals('August 15, 2020', $concert->formatted_date);
+    }
 
-        // Verify the date is formatted as expected
-        self::assertEquals('August 15, 2020', $date);
+    /** @test */
+    public function can_get_formatted_start_time(): void {
+        $concert = factory(Concert::class)->make(['date' => Carbon::parse('2020-08-15 21:30:00')]);
+
+        self::assertEquals('9:30pm', $concert->formatted_start_time);
+    }
+
+    /** @test */
+    public function can_get_ticket_price_in_dollars(): void {
+        $concert = factory(Concert::class)->make(['ticket_price' => 123456]);
+
+        self::assertEquals('1,234.56', $concert->ticket_price_in_dollars);
     }
 }
