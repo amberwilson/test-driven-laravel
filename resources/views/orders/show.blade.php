1ae8cca21032d0f3fe1dda1c0be62d41b8d16a70
amberwilson <678866+amberwilson@users.noreply.github.com>
2021-07-11 16:01:11 -0300

    Finish filling out and testing order view • COVERS https://course.testdrivenlaravel.com/lessons/module-12/making-static-data-real?autoplay=true#73 https://course.testdrivenlaravel.com/lessons/module-12/deciding-what-to-test-in-a-view#74 https://course.testdrivenlaravel.com/lessons/module-12/decoupling-data-from-presentation?autoplay=true#75

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Order;
 use App\Ticket;
 
@@ -17,7 +18,8 @@ use App\Ticket;
                 <div class="m-xs-b-6">
                     <div class="flex-baseline flex-spaced p-xs-y-4 border-b">
                         <h1 class="text-xl">Order Summary</h1>
-                        <a href="#" class="link-brand-soft">{{ $order->confirmation_number }}</a>
+                        <a href="{{ url("/orders/{$order->confirmation_number}") }}"
+                           class="link-brand-soft">{{ $order->confirmation_number }}</a>
                     </div>
                     <div class="p-xs-y-4 border-b">
                         <p>
@@ -29,51 +31,57 @@ use App\Ticket;
                 <div class="m-xs-b-7">
                     <h2 class="text-lg wt-normal m-xs-b-4">Your Tickets</h2>
                     @foreach($order->tickets as $ticket)
-                    <div class="card m-xs-b-5">
-                        <div class="card-section p-xs-y-3 flex-baseline flex-spaced text-light bg-gray">
-                            <div>
-                                <h1 class="text-xl wt-normal">No Warning</h1>
-                                <p class="text-light-muted">with Cruel Hand and Backtrack</p>
+                        <div class="card m-xs-b-5">
+                            <div class="card-section p-xs-y-3 flex-baseline flex-spaced text-light bg-gray">
+                                <div>
+                                    <h1 class="text-xl wt-normal">{{ $ticket->concert->title }}</h1>
+                                    <p class="text-light-muted">{{ $ticket->concert->subtitle }}</p>
+                                </div>
+                                <div class="text-right">
+                                    <strong>General Admission</strong>
+                                    <p class="text-light-soft">Admit one</p>
+                                </div>
                             </div>
-                            <div class="text-right">
-                                <strong>General Admission</strong>
-                                <p class="text-light-soft">Admit one</p>
-                            </div>
-                        </div>
-                        <div class="card-section border-b">
-                            <div class="row">
-                                <div class="col-sm">
-                                    <div class="media-object">
-                                        <div class="media-left">
-                                            <x-icon-calendar/>
-                                        </div>
-                                        <div class="media-body p-xs-l-4">
-                                            <p class="wt-bold">Sunday, October 16, 2011</p>
-                                            <p class="text-dark-soft">Doors at 8:00PM</p>
+                            <div class="card-section border-b">
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <div class="media-object">
+                                            <div class="media-left">
+                                                <x-icon-calendar/>
+                                            </div>
+                                            <div class="media-body p-xs-l-4">
+                                                <p class="wt-bold">
+                                                    <time datetime="{{ $ticket->concert->date->format('Y-m-d H:i') }}">
+                                                        {{ $ticket->concert->date->format('l, F jS, Y') }}
+                                                    </time>
+                                                </p>
+                                                <p class="text-dark-soft">Doors at {{ $ticket->concert->date->format('g:ia') }}</p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-sm">
-                                    <div class="media-object">
-                                        <div class="media-left">
-                                            <x-icon-location-marker/>
-                                        </div>
-                                        <div class="media-body p-xs-l-4">
-                                            <p class="wt-bold">Music Hall of Williamsburg</p>
-                                            <div class="text-dark-soft">
-                                                <p>123 Main St. W</p>
-                                                <p>Brooklyn, New York 14259</p>
+                                    <div class="col-sm">
+                                        <div class="media-object">
+                                            <div class="media-left">
+                                                <x-icon-location-marker/>
+                                            </div>
+                                            <div class="media-body p-xs-l-4">
+                                                <p class="wt-bold">{{ $ticket->concert->venue }}</p>
+                                                <div class="text-dark-soft">
+                                                    <p>{{ $ticket->concert->venue_address }}</p>
+                                                    <p>
+                                                        {{ $ticket->concert->city }}
+                                                        , {{ $ticket->concert->state }} {{ $ticket->concert->zip }}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="card-section flex-baseline flex-spaced">
+                                <p class="text-lg">{{ $ticket->code }}</p>
+                                <p>{{ $order->email }}</p>
+                            </div>
                         </div>
-                        <div class="card-section flex-baseline flex-spaced">
-                            <p class="text-lg">{{ $ticket->code }}</p>
-                            <p>adam.wathan@example.com</p>
-                        </div>
-                    </div>
                     @endforeach
                 </div>
                 <div class="text-center text-dark-soft wt-medium">
--- tests/Feature/ViewOrderTest.php
+++ tests/Feature/ViewOrderTest.php
@@ -6,6 +6,7 @@ use App\Concert;
 use App\Order;
 use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class ViewOrderTest extends TestCase
@@ -18,13 +19,26 @@ class ViewOrderTest extends TestCase
         $this->withoutExceptionHandling();
 
         /** @var Concert $concert */
-        $concert = factory(Concert::class)->create();
+        $concert = factory(Concert::class)->create(
+            [
+                'title' => 'The Red Chord',
+                'subtitle' => 'with Animosity and Lethargy',
+                'date' => Carbon::parse('March 12, 2017 8:00pm'),
+                'ticket_price' => 4250,
+                'venue' => 'The Mosh Pit',
+                'venue_address' => '123 Example Lane',
+                'city' => 'Laraville',
+                'state' => 'ON',
+                'zip' => '17916',
+            ]
+        );
         /** @var Order $order */
         $order = factory(Order::class)->create(
             [
                 'confirmation_number' => 'ORDERCONFIRMATION1234',
                 'card_last_four' => '1881',
                 'amount' => 8500,
+                'email' => 'jane@example.com',
             ]
         );
         $ticket1 = factory(Ticket::class)->create(
@@ -55,6 +69,15 @@ class ViewOrderTest extends TestCase
         $response->assertSee('**** **** **** 1881');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville');
+        $response->assertSee(', ON');
+        $response->assertSee('17916');
+        $response->assertSee('jane@example.com');
+        $response->assertSee('2017-03-12 20:00');
     }
 
 }
